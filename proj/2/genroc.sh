ec1c552305486595410a49d4506d4faa89af2dc6
Will Bartlett <wab73@cornell.edu>
2013-10-02 18:24:59 -0400

    script even better

--- proj/2/genroc.sh
+++ proj/2/genroc.sh
@@ -2,28 +2,32 @@
 EXE1=code/Features
 EXE2=samplesolution/F*x
 SDIR=imagesets
-SET=yosemite
+SET=graf
 ODIR=data
+PRE=img
+EXT=ppm
 
-$EXE1 computeFeatures $SDIR/$SET/$SET"1.jpg" $ODIR/"1hs.f" 2 1
-$EXE1 computeFeatures $SDIR/$SET/$SET"1.jpg" $ODIR/"1hm.f" 2 2
-$EXE1 computeFeatures $SDIR/$SET/$SET"2.jpg" $ODIR/"2hs.f" 2 1
-$EXE1 computeFeatures $SDIR/$SET/$SET"2.jpg" $ODIR/"2hm.f" 2 2
+rm $ODIR/*
 
-$EXE2 computeFeatures $SDIR/$SET/$SET"1.jpg" $ODIR/"1hsa.f" 2 1
-$EXE2 computeFeatures $SDIR/$SET/$SET"1.jpg" $ODIR/"1hma.f" 2 2
-$EXE2 computeFeatures $SDIR/$SET/$SET"2.jpg" $ODIR/"2hsa.f" 2 1
-$EXE2 computeFeatures $SDIR/$SET/$SET"2.jpg" $ODIR/"2hma.f" 2 2
+$EXE1 computeFeatures $SDIR/$SET/$PRE"1."$EXT $ODIR/"1hs.f" 2 1
+$EXE1 computeFeatures $SDIR/$SET/$PRE"1."$EXT $ODIR/"1hm.f" 2 2
+$EXE1 computeFeatures $SDIR/$SET/$PRE"2."$EXT $ODIR/"2hs.f" 2 1
+$EXE1 computeFeatures $SDIR/$SET/$PRE"2."$EXT $ODIR/"2hm.f" 2 2
 
-$EXE1 roc $ODIR/"1hs.f" $ODIR/"2hs.f" $SDIR/$SET/"H1to2p" 1 rocfile1hs aucfile1hs
-$EXE1 roc $ODIR/"1hm.f" $ODIR/"2hm.f" $SDIR/$SET/"H1to2p" 1 rocfile1hm aucfile1hm
-$EXE1 roc $ODIR/"1hs.f" $ODIR/"2hs.f" $SDIR/$SET/"H1to2p" 2 rocfile2hs aucfile2hs
-$EXE1 roc $ODIR/"1hm.f" $ODIR/"2hm.f" $SDIR/$SET/"H1to2p" 2 rocfile2hm aucfile2hm
+$EXE2 computeFeatures $SDIR/$SET/$PRE"1."$EXT $ODIR/"1hsa.f" 2 1
+$EXE2 computeFeatures $SDIR/$SET/$PRE"1."$EXT $ODIR/"1hma.f" 2 2
+$EXE2 computeFeatures $SDIR/$SET/$PRE"2."$EXT $ODIR/"2hsa.f" 2 1
+$EXE2 computeFeatures $SDIR/$SET/$PRE"2."$EXT $ODIR/"2hma.f" 2 2
 
-$EXE2 roc $ODIR/"1hsa.f" $ODIR/"2hsa.f" $SDIR/$SET/"H1to2p" 1 rocfile1hsa aucfile1hsa
-$EXE2 roc $ODIR/"1hma.f" $ODIR/"2hma.f" $SDIR/$SET/"H1to2p" 1 rocfile1hma aucfile1hma
-$EXE2 roc $ODIR/"1hsa.f" $ODIR/"2hsa.f" $SDIR/$SET/"H1to2p" 2 rocfile2hsa aucfile2hsa
-$EXE2 roc $ODIR/"1hma.f" $ODIR/"2hma.f" $SDIR/$SET/"H1to2p" 2 rocfile2hma aucfile2hma
+$EXE1 roc $ODIR/"1hs.f" $ODIR/"2hs.f" $SDIR/$SET/"H1to2p" 1 $ODIR/rocfile1hs $ODIR/aucfile1hs
+$EXE1 roc $ODIR/"1hm.f" $ODIR/"2hm.f" $SDIR/$SET/"H1to2p" 1 $ODIR/rocfile1hm $ODIR/aucfile1hm
+$EXE1 roc $ODIR/"1hs.f" $ODIR/"2hs.f" $SDIR/$SET/"H1to2p" 2 $ODIR/rocfile2hs $ODIR/aucfile2hs
+$EXE1 roc $ODIR/"1hm.f" $ODIR/"2hm.f" $SDIR/$SET/"H1to2p" 2 $ODIR/rocfile2hm $ODIR/aucfile2hm
+
+$EXE2 roc $ODIR/"1hsa.f" $ODIR/"2hsa.f" $SDIR/$SET/"H1to2p" 1 $ODIR/rocfile1hsa $ODIR/aucfile1hsa
+$EXE2 roc $ODIR/"1hma.f" $ODIR/"2hma.f" $SDIR/$SET/"H1to2p" 1 $ODIR/rocfile1hma $ODIR/aucfile1hma
+$EXE2 roc $ODIR/"1hsa.f" $ODIR/"2hsa.f" $SDIR/$SET/"H1to2p" 2 $ODIR/rocfile2hsa $ODIR/aucfile2hsa
+$EXE2 roc $ODIR/"1hma.f" $ODIR/"2hma.f" $SDIR/$SET/"H1to2p" 2 $ODIR/rocfile2hma $ODIR/aucfile2hma
 
 gnuplot plot.roc.txt
 gnuplot plota.roc.txt
