1310e9c5c1a54b7871709524fdc394003fb41681
Will Bartlett <wab73@cornell.edu>
2013-11-04 15:06:07 -0500

    pushing

--- proj/3/testsets/pano_test/genFeaturesMatchScript.sh
+++ proj/3/testsets/pano_test/genFeaturesMatchScript.sh
@@ -12,6 +12,6 @@ awk '{ for (i=1; i <= NF; i++) { \
 
 awk '{ for (i=1; i <= NF; i++) { \
          j = i % NF + 1; \
-         print "echo \"Aligning image " i " to " j "\"\n../../Panorama.exe alignPair " $i, $j " match_" i "_" j ".txt 1000 4.0 | tail -1 > align_" i "_" j ".txt\necho `basename " $i " .f`.tga `basename " $j " .f`.tga `cat align_" i "_" j ".txt` >> pairlist.txt"; \
+         print "echo \"Aligning image " i " to " j "\"\n../Panorama.exe alignPair " $i, $j " match_" i "_" j ".txt 1000 4.0 | tail -1 > align_" i "_" j ".txt\necho `basename " $i " .f`.tga `basename " $j " .f`.tga `cat align_" i "_" j ".txt` >> pairlist.txt"; \
        } \
      } ' files_one_line.txt
--- proj/3/testsets/pano_test/genSIFTMatchScript.sh
+++ proj/3/testsets/pano_test/genSIFTMatchScript.sh
@@ -12,6 +12,6 @@ awk '{ for (i=1; i <= NF; i++) { \
 
 awk '{ for (i=1; i <= NF; i++) { \
          j = i % NF + 1; \
-         print "echo \"Aligning image " i " to " j "\"\n../../Panorama.exe alignPair " $i, $j " match_" i "_" j ".txt 1000 4.0 sift | tail -1 > align_" i "_" j ".txt\necho `basename " $i " .key`.tga `basename " $j " .key`.tga `cat align_" i "_" j ".txt` >> pairlist.txt"; \
+         print "echo \"Aligning image " i " to " j "\"\n../Panorama.exe alignPair " $i, $j " match_" i "_" j ".txt 1000 4.0 sift | tail -1 > align_" i "_" j ".txt\necho `basename " $i " .key`.tga `basename " $j " .key`.tga `cat align_" i "_" j ".txt` >> pairlist.txt"; \
        } \
      } ' files_one_line.txt
